5e441050ee00ff650e89129dfbc96deb7716d623
Fyllus <fylluszophia@proton.me>
2026-07-11 23:40:46 -0300

    chore: updating main branch workflow

--- src/goastty/__project__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.4.0"
-__project__ = "go-astty"
-__description__ = "go-astty is modular process orchestration gate for seamless synchronous and asynchronous command-line executions in Python."
-__author__ = "Fyllus"
-__email__ = "fylluszophia@proton.me"
-__release__ = {"0.4.0": ""}
